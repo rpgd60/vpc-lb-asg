d98085e18f08085e15f4ebceaf970e9eeb1c5dbe
Rafa <rafa@portillo.eu>
2022-11-13 23:38:40 +0100

    deleted old scripts

--- create.sh
+++
@@ -1,8 +0,0 @@
-aws cloudformation create-stack --stack-name vpc$1  --template-body file://rp-vpc.yaml \
- --parameters ParameterKey=EnvironmentName,ParameterValue=vpc$1 \
- ParameterKey=VpcCIDR,ParameterValue=10.$1.0.0/16 \
- ParameterKey=CreateNatGateways,ParameterValue=true \
- ParameterKey=SingleNatGateway,ParameterValue=true \
- ParameterKey=CreateSSMVPCEndpoints,ParameterValue=false \
- ParameterKey=CreateALBandASG,ParameterValue=true \
- ParameterKey=CreateBastion,ParameterValue=true 
